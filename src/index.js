2e5071e95a691543f981b5ee00c5ec98fd632de5
Aleksandr Savchenko <titamoto@gmail.com>
2023-04-10 14:02:23 -0700

    basic tasks complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submit = document.getElementById('create-task-form');
+  submit.addEventListener('submit', (e) => { 
+  e.preventDefault();
+  handleToDo(e.target['new-task-description'].value);
 });
+});
+
+function handleToDo(todo) {
+  const li = document.createElement('li');
+  li.textContent = todo;
+  document.getElementById('tasks').appendChild(li);
+}
